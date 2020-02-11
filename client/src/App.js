45154f4180589aa0a20139f9550853de3fa75093
A Tawfique <easternselling@gmail.com>
2020-02-11 11:11:32 -0500

    Scraper is basically done now. Exception handling added.

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,46 @@
 import React from 'react';
 import './App.css';
+import Scraper from './test.js'
+import Axios from 'axios'
 
 class App extends React.Component {
   constructor(props) {
     super(props)
 
-    state = {
-
+    this.state = {
+      ASIN: ""
     }
+  }
+
+  handleChange = (e) => {
+    const { value } = e.target;
+    this.setState({
+      ASIN: value
+    })
+    console.log(this.state.ASIN)
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    console.log('hi')
+    Axios.get(`http://localhost:3001/${this.state.ASIN}`)
   }
 
   render() {
     return (
       <div className="App" >
-
-      </div>
+        <form onSubmit={(e) => { this.handleSubmit(e) }}>
+          <input
+            type="text"
+            onChange={(e) => { this.handleChange(e) }}
+            onSubmit={(e) => { this.handleSubmit(e) }}
+          />
+          <input
+            type="submit"
+            onSubmit={(e) => { this.handleSubmit(e) }}
+          />
+        </form>
+      </div >
     );
   }
 }
