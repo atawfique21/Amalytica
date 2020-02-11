9bdff34d78a98b70ec11052251c52b5c1460559a
A Tawfique <easternselling@gmail.com>
2020-02-11 13:35:07 -0500

    Sending back correct object now for currentUser. Authentication with JWT fully finished.

--- back_end/app/auth/authenticate_user.rb
+++ back_end/app/auth/authenticate_user.rb
@@ -7,7 +7,7 @@ class AuthenticateUser
 
   # Service entry point
   def call
-    JsonWebToken.encode(user_id: user.id) if user
+    [JsonWebToken.encode(user_id: user.id), user.name, user.username] if user
   end
 
   private
--- back_end/app/controllers/authentication_controller.rb
+++ back_end/app/controllers/authentication_controller.rb
@@ -4,7 +4,7 @@ class AuthenticationController < ApplicationController
   def authenticate
     auth_token =
         AuthenticateUser.new(auth_params[:username], auth_params[:password]).call
-    json_response(auth_token: auth_token)
+    json_response(auth_token: auth_token[0], name: auth_token[1], username: auth_token[2])
   end
 
   private
