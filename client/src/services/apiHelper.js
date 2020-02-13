78b230c28dca36ea9003dd277429ae121064e285
A Tawfique <easternselling@gmail.com>
2020-02-13 11:37:56 -0500

    Now verifying user to persist session.

--- client/src/services/apiHelper.js
+++ client/src/services/apiHelper.js
@@ -6,14 +6,25 @@ const api = Axios.create({
 
 export const loginUser = async (loginData) => {
   const resp = await api.post('/auth/login', loginData);
-  localStorage.setItem('authToken', resp.data.token);
+  localStorage.setItem('authToken', resp.data.auth_token);
+  localStorage.setItem('name', resp.data.name);
+  localStorage.setItem('username', resp.data.email);
   api.defaults.headers.common.authorization = `Bearer ${resp.data.token}`;
   return resp.data;
 }
 
 export const registerUser = async (registerData) => {
   const resp = await api.post('/signup', registerData);
-  localStorage.setItem('authToken', resp.data.token);
+  localStorage.setItem('authToken', resp.data.auth_token);
+  localStorage.setItem('name', resp.data.name);
+  localStorage.setItem('username', resp.data.email);
   api.defaults.headers.common.authorization = `Bearer ${resp.data.token}`;
   return resp.data;
+}
+
+export const verifyUser = () => {
+  const token = localStorage.getItem('authToken');
+  if (token) {
+    api.defaults.headers.common.authorization = `Bearer ${token}`;
+  }
 }
