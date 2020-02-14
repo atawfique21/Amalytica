25e16cd25415b23a4ec6f4cd031920479609dd96
A Tawfique <easternselling@gmail.com>
2020-02-14 17:09:21 -0500

    Changed getVitals function to correct endpoint.

--- client/src/services/seleniumHelper.js
+++ client/src/services/seleniumHelper.js
@@ -6,14 +6,15 @@ const api = Axios.create({
 
 export const getVitals = async (ASIN, user_id) => {
   user_id = parseInt(user_id)
-  const resp = await api.get(`/${ASIN}`);
+  const resp = await api.get(`/vitals/${ASIN}`);
+  console.log(resp)
   // console.log(resp.data.rel)
   let productData = {
     asin: ASIN,
-    image: resp.data.img,
+    image: resp.data.image,
     title: resp.data.title,
     user_id: user_id,
-    price: resp.data.price
+    price: resp.data.buyboxprice
   }
   // productData = JSON.stringify(productData)
 
