dbd887cedd7b802eb07d3b06928c8c65d431ec14
A Tawfique <easternselling@gmail.com>
2020-02-14 13:39:07 -0500

    Added price column to product to update latest price.

--- back_end/db/schema.rb
+++ back_end/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_13_174411) do
+ActiveRecord::Schema.define(version: 2020_02_14_182653) do
 
   create_table "products", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_02_13_174411) do
     t.integer "ratings"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "price"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
