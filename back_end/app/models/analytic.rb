0943556f9a2fc45b5653a8a2d46018e1b564c5cb
A Tawfique <easternselling@gmail.com>
2020-02-17 11:27:58 -0500

    Now requiring product asin and uppercasing ASIN before saving to Db.

--- back_end/app/models/analytic.rb
+++ back_end/app/models/analytic.rb
@@ -1,3 +1,8 @@
 class Analytic < ApplicationRecord
-  validates_presence_of :price, :available, :condition, :seller
+  validates_presence_of :price, :available, :condition, :seller, :product_asin
+  belongs_to :product, foreign_key: 'product_asin'
+
+  def before_save
+    self.product_asin  = self.product_asin.upcase!
+  end
 end
