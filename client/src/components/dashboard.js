19cd551f2342ccaf9f70ea839b604e59d43ab8dd
A Tawfique <easternselling@gmail.com>
2020-02-19 10:53:15 -0500

    Added ReCharts

--- client/src/components/dashboard.js
+++ client/src/components/dashboard.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { getVitals, checkDuplicate, getBuyBox, getOffer } from '../services/seleniumHelper'
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts'
 import search from '../assets/search.png'
 
 class Dashboard extends React.Component {
@@ -9,14 +10,112 @@ class Dashboard extends React.Component {
     this.state = {
       ASIN: null,
       currentProducts: [],
-      currentBuyBoxes: [],
       vitalDataLoading: false,
       buyBoxDataLoading: false,
       offerOneDataLoading: false,
       offerTwoDataLoading: false,
-      buyBoxCounter: -1,
       errorText: false
     }
+  }
+
+  renderChart = (product) => {
+    let final = []
+    product.analytics.map(analytic => {
+      let index = final.findIndex(obj => obj.seller === this.findSeller(analytic.seller))
+      if (index === -1) {
+        let ls = { seller: this.findSeller(analytic.seller), data: [] }
+        for (let i = 0; i < product.analytics.length; i++) {
+          if (analytic.seller === product.analytics[i].seller) {
+            let newDate = Date.parse(product.analytics[i].created_at)
+            let jsDate = new Date(newDate)
+
+            let day = jsDate.getDate()
+            let getmonth = jsDate.getMonth()
+            let hour = jsDate.getHours()
+            let minute = jsDate.getMinutes()
+
+            var month = new Array();
+            month[0] = "January";
+            month[1] = "February";
+            month[2] = "March";
+            month[3] = "April";
+            month[4] = "May";
+            month[5] = "June";
+            month[6] = "July";
+            month[7] = "August";
+            month[8] = "September";
+            month[9] = "October";
+            month[10] = "November";
+            month[11] = "December";
+
+            month = month[getmonth]
+            month = month[0] + month[1] + month[2]
+
+            newDate = `${month} ${day}`
+            let editedPrice = product.analytics[i].price.split('').slice(1, 6).join('')
+
+            let ms = { date: newDate, price: parseFloat(editedPrice) }
+            ls.data.push(ms)
+          }
+        }
+        final.push(ls)
+      }
+    })
+    product.buy_boxes.map(buybox => {
+      let index = final.findIndex(obj => obj.seller === this.findSeller(buybox.seller))
+      if (index === -1) {
+        let ls = { seller: this.findSeller(buybox.seller), data: [] }
+        for (let i = 0; i < product.buy_boxes.length; i++) {
+          if (buybox.seller === product.buy_boxes[i].seller) {
+            var newDate = Date.parse(product.buy_boxes[i].created_at)
+            let jsDate = new Date(newDate)
+
+            let day = jsDate.getDate()
+            let getmonth = jsDate.getMonth()
+            let hour = jsDate.getHours()
+            let minute = jsDate.getMinutes()
+
+            var month = new Array();
+            month[0] = "January";
+            month[1] = "February";
+            month[2] = "March";
+            month[3] = "April";
+            month[4] = "May";
+            month[5] = "June";
+            month[6] = "July";
+            month[7] = "August";
+            month[8] = "September";
+            month[9] = "October";
+            month[10] = "November";
+            month[11] = "December";
+
+            month = month[getmonth]
+            month = month[0] + month[1] + month[2]
+
+            newDate = `${month} ${day}`
+            let editedPrice = product.buy_boxes[i].price.split('').slice(1, 6).join('')
+
+            let ms = { date: newDate, price: parseFloat(editedPrice) }
+            ls.data.push(ms)
+          }
+        }
+        final.push(ls)
+      }
+    })
+
+    var colorArray = ['#FF6633', '#FF33FF', '#00B3E6', '#999966', '#99FF99', '#B34D4D', '#80B300', '#809900', '#E6B3B3', '#6680B3', '#66991A']
+    return (
+      <LineChart width={430} height={215}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date" type="category" allowDuplicatedCategory={true} />
+        <YAxis dataKey="price" domain={[(dataMin) => parseInt(dataMin - 2), (dataMax) => parseInt(dataMax + 2)]} allowDataOverflow={true} />
+        <Tooltip />
+        <Legend />
+        {final.map((analytics, index) => (
+          <Line dataKey="price" data={analytics.data} name={analytics.seller} key={analytics.seller} stroke={colorArray[index]} />
+        ))}
+      </LineChart>
+    )
   }
 
   handleChange = (e) => {
@@ -24,6 +123,31 @@ class Dashboard extends React.Component {
     this.setState({
       ASIN: value
     })
+  }
+
+  findSeller = (string) => {
+    let stringArr = string.split(' ')
+    if (stringArr[0] === 'Ships') {
+      let afterBy = string.split('by').pop().trim();
+      let afterBySplit = afterBy.split('')
+      if (afterBySplit[afterBySplit.length - 1] === '.') {
+        afterBySplit.pop()
+        afterBy = afterBySplit.join('')
+        return afterBy
+      } else {
+        return afterBy;
+      }
+    } else if (stringArr[0] === 'Sold') {
+      let first = string.split('Sold by').pop()
+      let second = first.split('and').shift().trim();
+      return second;
+    } else if (stringArr[0] === 'Seller:') {
+      let first = string.split('Seller:').pop()
+      let second = first.split(',').shift().trim();
+      return second;
+    } else {
+      return string
+    }
   }
 
   handleSubmit = async (e) => {
@@ -104,15 +228,6 @@ class Dashboard extends React.Component {
       return numbers
     }
 
-    function lastWord(string) {
-      let n = string.split(" ");
-      n = n[n.length - 1];
-      if (n[n.length - 1] === ".") {
-        return n.slice(0, -1)
-      } else {
-        return n
-      }
-    }
     return (
       <div>
         {
@@ -145,21 +260,34 @@ class Dashboard extends React.Component {
                   <h3>{this.state.errorText}</h3>
                 </div>
               }
-              {
-                this.state.vitalDataLoading &&
-                <div className="single-data loading-wrapper">
-                  <div className="loader">
-                    <div className="line line1"></div>
-                    <div className="line line2"></div>
-                    <div className="line line3"></div>
-                    <div className="line line4"></div>
-                    <div className="line line5"></div>
-                  </div>
-                  <h3>Getting vital product data for ASIN:</h3>
-                  <h4>{this.state.ASIN}</h4>
-                </div>
-              }
               <div className="data-wrapper">
+                {
+                  this.props.refreshing &&
+                  <div className="single-data loading-wrapper">
+                    <div className="loader">
+                      <div className="line line1"></div>
+                      <div className="line line2"></div>
+                      <div className="line line3"></div>
+                      <div className="line line4"></div>
+                      <div className="line line5"></div>
+                    </div>
+                    <h3>Refreshing Data</h3>
+                  </div>
+                }
+                {
+                  this.state.vitalDataLoading &&
+                  <div className="single-data loading-wrapper">
+                    <div className="loader">
+                      <div className="line line1"></div>
+                      <div className="line line2"></div>
+                      <div className="line line3"></div>
+                      <div className="line line4"></div>
+                      <div className="line line5"></div>
+                    </div>
+                    <h3>Getting vital product data for ASIN:</h3>
+                    <h4>{this.state.ASIN}</h4>
+                  </div>
+                }
                 {this.state.currentProducts && this.state.currentProducts.map((product, key) =>
                   <div className="single-data" key={key}>
                     <h1>{product.title}</h1>
@@ -192,7 +320,7 @@ class Dashboard extends React.Component {
                               <h4>{findNumbers(buybox.available)}</h4>
                             </div>
                           </div>
-                          <h5>Sold By {lastWord(buybox.seller)}</h5>
+                          <h5>Sold By {this.findSeller(buybox.seller)}</h5>
                         </div>
                       )
                     }
@@ -223,7 +351,7 @@ class Dashboard extends React.Component {
                             </div>
                           </div>
                           <h5>{offer.condition} condition</h5>
-                          <h5>Sold By {lastWord(offer.seller)}</h5>
+                          <h5>Sold By {this.findSeller(offer.seller)}</h5>
                         </div>
                       )
                     }
@@ -243,48 +371,64 @@ class Dashboard extends React.Component {
                 )}
                 {this.props.currentProducts &&
                   this.props.currentProducts.map((product, key) =>
-                    <div className="single-data" key={key}>
+                    < div className="single-data" key={key} >
                       <h1>{product.title}</h1>
                       <h2>{product.asin}</h2>
                       <img src={product.image}></img>
                       <h2>{product.price}</h2>
-                      {product.buy_boxes &&
-                        product.buy_boxes.map((buybox, key) =>
-                          <div className="single-buy-box" key={key}>
-                            <h4>Buy Box</h4>
-                            <div className="buy-box-details-wrapper">
-                              <div className="buy-box-details">
-                                <h3>Price</h3>
-                                <h4>{buybox.price}</h4>
-                              </div>
-                              <div className="buy-box-details">
-                                <h3>Available</h3>
-                                <h4>{findNumbers(buybox.available)}</h4>
-                              </div>
-                            </div>
-                            <h5>Sold By {lastWord(buybox.seller)}</h5>
-                          </div>
+                      <h3>Price History</h3>
+                      <h6>VIA SELLER OVER TIME</h6>
+                      {this.props.currentProducts && this.renderChart(product)}
+                      {
+                        product.buy_boxes &&
+                        product.buy_boxes.map((buybox, key, arr) => {
+                          if (arr.length - 1 === key) {
+                            return (
+                              <div className="single-buy-box" key={key}>
+                                <h4>Buy Box</h4>
+                                <div className="buy-box-details-wrapper">
+                                  <div className="buy-box-details">
+                                    <h3>Price</h3>
+                                    <h4>{buybox.price}</h4>
+                                  </div>
+                                  <div className="buy-box-details">
+                                    <h3>Available</h3>
+                                    <h4>{findNumbers(buybox.available)}</h4>
+                                  </div>
+                                </div>
+                                <h5>Sold By {this.findSeller(buybox.seller)}</h5>
+                              </div>)
+                          }
+                        }
+
                         )
                       }
-                      {product.analytics &&
-                        product.analytics.map((offer, key) =>
-                          <div className="single-buy-box" key={key}>
-                            <h4>Offer {key + 1}</h4>
-                            <div className="buy-box-details-wrapper">
-                              <div className="buy-box-details">
-                                <h3>Price</h3>
-                                <h4>{offer.price}</h4>
+                      {
+                        product.analytics &&
+                        product.analytics.map((offer, key, arr) => {
+                          if (arr.length - 2 === key || arr.length - 1 === key) {
+                            return (
+                              <div className="single-buy-box" key={key}>
+                                <h4>Offer {key + 1}</h4>
+                                <div className="buy-box-details-wrapper">
+                                  <div className="buy-box-details">
+                                    <h3>Price</h3>
+                                    <h4>{offer.price}</h4>
+                                  </div>
+                                  <div className="buy-box-details">
+                                    <h3>Available</h3>
+                                    <h4>{findNumbers(offer.available)}</h4>
+                                  </div>
+                                </div>
+                                <h5>{offer.condition} condition</h5>
+                                <h5>Sold By {this.findSeller(offer.seller)}</h5>
                               </div>
-                              <div className="buy-box-details">
-                                <h3>Available</h3>
-                                <h4>{findNumbers(offer.available)}</h4>
-                              </div>
-                            </div>
-                            <h5>{offer.condition} condition</h5>
-                            <h5>Sold By {lastWord(offer.seller)}</h5>
-                          </div>
+                            )
+                          }
+                        }
                         )
                       }
+                      <button onClick={(e) => this.props.handleRefresh(e, product)} />
                     </div>
                   )
                 }
